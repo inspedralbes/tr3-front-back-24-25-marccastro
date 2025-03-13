03dd25d613b94a6a6e6ffbd12d4cc36b034ff3d1
a18marcastru <a18marcastru@inspedralbes.cat>
2025-03-13 19:08:34 +0100

    Canvis.

--- Front/vuetify-boxhead/src/components/Boxhead-Control.vue
+++ Front/vuetify-boxhead/src/components/Boxhead-Control.vue
@@ -16,45 +16,11 @@
                 Ajusta los atributos de los personajes en tiempo real.
               </v-card-subtitle>
               <v-card-text>
-                 
-                <v-select
-                  v-model="selectedEnemy"
-                  :items="enemies"
-                  label="Seleccionar Enemigo"
-                  outlined
-                ></v-select>
-
-                <v-slider
-                  v-model="health"
-                  label="Health"
-                  :min="0"
-                  :max="100"
-                  step="1"
-                  thumb-label
-                ></v-slider>
-                <v-slider
-                  v-model="speed"
-                  label="Speed"
-                  :min="0"
-                  :max="10"
-                  step="0.1"
-                  thumb-label
-                ></v-slider>
-                <v-slider
-                  v-model="damage"
-                  label="Damage"
-                  :min="10"
-                  :max="100"
-                  step="5"
-                  thumb-label
-                ></v-slider>
-
-                <v-select
-                  v-model="sprite"
-                  :items="sprites"
-                  label="Sprite"
-                  outlined
-                ></v-select>
+                <v-select v-model="selectedEnemy" :items="enemies" label="Seleccionar Enemigo" outlined></v-select>
+                <v-slider v-model="health" label="Health" :min="1" :max="100" step="1" thumb-label></v-slider>
+                <v-slider v-model="speed" label="Speed" :min="1" :max="10" step="0" thumb-label></v-slider>
+                <v-slider v-model="damage" label="Damage" :min="10" :max="100" step="5" thumb-label></v-slider>
+                <v-select v-model="sprite" :items="sprites" label="Sprite"></v-select>
               </v-card-text>
               <v-card-actions>
                 <v-btn @click="updateCharacter">Actualizar Personaje</v-btn>
@@ -71,20 +37,15 @@
 import { ref } from 'vue';
 import { functionSocket } from '../services/socketManager';
 
-const health = ref(50); // Valor predeterminado
-const speed = ref(5); // Valor predeterminado
-const damage = ref(20); // Valor predeterminado
-const sprite = ref(null);
+const health = ref(0);
+const speed = ref(0);
+const damage = ref(0);
 const selectedEnemy = ref(null); // Nombre del enemigo seleccionado
 const enemies = ref(['Zombie', 'DogZombie']); // Enemigos disponibles
-const sprites = ref(['Sprite1', 'Sprite2', 'Sprite3']); // Sprites disponibles
+const sprite = ref(null);
+const sprites = ref(['Sprite1', 'Sprite2', 'Sprite3']);
 
 const updateCharacter = () => {
-  if (selectedEnemy.value) {
-    // Llamamos a la función para enviar los datos al servidor
-    functionSocket(health, speed, damage, sprite);
-  } else {
-    console.error("Por favor selecciona un enemigo.");
-  }
-};
+  functionSocket(selectedEnemy, health, speed, damage);
+}
 </script>
--- Front/vuetify-boxhead/src/services/socketManager.js
+++ Front/vuetify-boxhead/src/services/socketManager.js
@@ -5,10 +5,10 @@ socket.onopen = () => {
     console.log("Conectado al servidor WebSocket");
 };
 
-export const functionSocket = (enemyName, newHealth, newSpeed, newDamage) => {
+export const functionSocket = (enemy, newHealth, newSpeed, newDamage) => {
   if (socket.readyState === WebSocket.OPEN) {
     console.log("Enviado");
-    const data = { health: newHealth.value, speed: newSpeed.value, damage: newDamage.value }; 
+    const data = { name: enemy.value, health: newHealth.value, speed: newSpeed.value, damage: newDamage.value }; 
     socket.send(JSON.stringify(data));
   } else {
     console.error("WebSocket no está conectado.");
